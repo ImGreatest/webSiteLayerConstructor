bc401c0d104f576ac9a18fe24855db108cac1684
Deserag <pavelkopytov472@gmail.com>
2023-12-07 16:52:43 +0300

    Добавил создание кнопок в конструкторе а также изменение их свойств

--- src/app/maket/maket.component.ts
+++ src/app/maket/maket.component.ts
@@ -1,10 +1,11 @@
-import {Component, AfterViewInit} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import grapesjs from "grapesjs";
-import 'grapesjs-preset-webpage';
-import {Router} from "@angular/router";
-
 import 'grapesjs-plugin-export';
-//
+import 'grapesjs-lory-slider';
+import 'grapesjs-custom-code';
+import 'grapesjs-tabs';
+import 'grapesjs-blocks-basic';
+
 @Component({
   selector: 'app-maket',
   standalone: true,
@@ -12,13 +13,82 @@ import 'grapesjs-plugin-export';
   templateUrl: './maket.component.html',
   styleUrl: './maket.component.css'
 })
-export class MaketComponent implements AfterViewInit{
+export class MaketComponent implements OnInit, AfterViewInit {
+  editor: any;
 
-  constructor() { }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    const editor = grapesjs.init({
-      container: '#editor',
+    this.editor = grapesjs.init({
+      container: '#gjs',
+      plugins: [
+        'gjs-preset-webpage',
+        'gjs-blocks-basic',
+        'gjs-plugin-export',
+        'gjs-lory-slider',
+        'gjs-custom-code',
+        'gjs-tabs'
+      ],
+      pluginsOpts: {
+        'gjs-blocks-basic': {
+          blocks: {
+            additional: {
+              label: 'Additional',
+              category: 'Extra',
+              attributes: { class: 'gjs-block-section' },
+              content: {
+                type: 'section',
+                content: `<h1>Additional Block</h1>`
+              }
+            }
+          }
+        }
+      },
+      canvas: {
+        styles: [
+          'https://unpkg.com/grapesjs/dist/css/grapes.min.css'
+        ]
+      }
     });
+
+    this.createMenu();
+  }
+
+  createMenu(): void {
+    const panelTop = document.createElement('div');
+    panelTop.className = 'panel__top';
+    document.body.appendChild(panelTop);
+
+    const btnSave = document.createElement('button');
+    btnSave.id = 'btn-save';
+    btnSave.className = 'btn-save';
+    btnSave.textContent = 'Save';
+    btnSave.addEventListener('click', () => {
+      const html = this.editor.getHtml();
+      const css = this.editor.getCss();
+      console.log(html);
+      console.log(css);
+      // Здесь вы можете выполнить дополнительные действия с экспортированным HTML и CSS
+    });
+    panelTop.appendChild(btnSave);
+
+    const btnAddButton = document.createElement('button');
+    btnAddButton.id = 'btn-add-button';
+    btnAddButton.className = 'btn-add-button';
+    btnAddButton.textContent = 'Add Button';
+    btnAddButton.addEventListener('click', () => {
+      const button = this.editor.DomComponents.addComponent({
+        tagName: 'button',
+        content: 'Click me',
+        style: {}
+      });
+      const selectedComponent = this.editor.getSelected();
+      if (selectedComponent) {
+        selectedComponent.addChild(button);
+      } else {
+        this.editor.getModel().get('components').add(button);
+      }
+    });
+    panelTop.appendChild(btnAddButton);
   }
 }
