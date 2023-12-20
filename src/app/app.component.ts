eb8845e3dd54cfc8925d2f00326c0df6ab650138
ImGreatest <ImGreatest@users.noreply.github.com>
2023-12-20 16:34:55 +0300

    update settings project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, RouterOutlet} from '@angular/router';
-import {TuiRootModule} from "@taiga-ui/core";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet, TuiRootModule],
+  imports: [CommonModule, RouterModule, RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {
   projectNameTitle = 'webSiteLayerConstructor';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,17 @@
-import { Routes } from '@angular/router';
-
+import {RouterModule, Routes} from '@angular/router';
+import {SchemasComponent} from "./schemas/schemas.component";
+import {ConstructorComponent} from "./constructor/constructor.component";
+import {NgModule} from "@angular/core";
+import {HomeComponent} from "./home/home.component";
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: "full"},
-  { path: 'contact', redirectTo: 'home'}
+  { path: '', component: HomeComponent }, // main page
+  { path: 'schemas', component: SchemasComponent }, // page of schemas
+  { path: 'constructor', component: ConstructorComponent }, // layout grapes.js
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutes {}
