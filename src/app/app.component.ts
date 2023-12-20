d655a1b31e00f24f0915a735242a1dce79267e0e
ImGreatest <ImGreatest@users.noreply.github.com>
2023-12-20 16:44:47 +0300

    change scheme project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,43 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule, RouterOutlet} from '@angular/router';
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import {BrowserModule} from "@angular/platform-browser";
+import { SharedDataService } from "./shared-data.service";
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+  TuiAlertService,
+  TuiSvgModule, TuiModeModule, TuiGroupModule
+} from "@taiga-ui/core";
+import {Component, Inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {TuiAvatarModule, TuiInputCountModule, TuiMarkerIconModule, TuiTabsModule} from "@taiga-ui/kit";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [CommonModule, RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiTabsModule, TuiSvgModule, FormsModule, TuiInputCountModule, TuiModeModule, TuiAvatarModule, TuiGroupModule, RouterLinkActive, RouterLink, TuiMarkerIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
-    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+  providers: [SharedDataService, {provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
 })
+
 export class AppComponent {
-  projectNameTitle = 'webSiteLayerConstructor';
-  protected readonly String = String;
+  activeItemIndex = 0;
+  isShow = false;
+
+  constructor(
+    private sharedData: SharedDataService,
+    @Inject(TuiAlertService)
+    private router: Router, readonly alerts: TuiAlertService,
+  ) {
+    this.sharedData.title = 'pages';
+    this.sharedData.activeItemIndex = 0;
+  }
+
+  navigateToHome() {
+    this.router.navigate(['']);
+  }
+
 }
